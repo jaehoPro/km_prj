c5e17b8814cd19c90c5096693d62d0da49c3cedd
kosmo-14 <kosmo-14@DESKTOP-594F3QN>
2018-12-20 16:21:50 +0900

    12.20 config mybatis -lodging 포함

--- src/com/km/lodging/LodgingDAO.java
+++ src/com/km/lodging/LodgingDAO.java
@@ -1,5 +1,19 @@
 package com.km.lodging;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.km.common.MyBatisFactory;
+
+
 public class LodgingDAO {
+	public LodgingVO memberLogin(LodgingVO lvo)
+	{
+		SqlSession conn = null;
+		
+		conn = MyBatisFactory.getFactory().openSession();
+		lvo=conn.selectOne("userNameSpace.selectOneLodging", lvo);
+
+		return lvo;
+	}
 
 }
